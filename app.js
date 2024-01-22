5908dcb30ad6b1a2f5d16976036d3f5cf7c3eab6
mknjbhvh <camila1034_@hotmail.com>
2024-01-21 23:24:56 -0500

    Se adjuntan los comentarios correspondientes al codigo

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const functions = require('./functions.js')
+const functions = require('./functions.js')//importando el módulo functions.js y asignándolo a la variable functions
 
-console.log(functions);
-process.stdout.write("Ejercicios/n/n");
+console.log(functions);//Se imprime en la consola el objeto functions ,esto permite ver qué funciones están disponibles en ese módulo.
+process.stdout.write("Ejercicios\n\n");//se utiliza para escribir directamente en la salida estándar se esta imprimiendo el texto "Ejercicios" seguido de dos saltos de línea).
 
-console.log(functions.add(5,3));
-console.log(functions.substract(2,8));
-console.log(functions.multiply(2,8));
-console.log(functions.divide(2,0.9));
+console.log(functions.add(5,3));//se utiliza la función add del objeto functions para sumar y luego se imprime el resultado en la consola.
+console.log(functions.substract(2,8));//se utiliza la función substract del objeto functions para restar y luego se imprime el resultado en la consola.
+console.log(functions.multiply(2,8));//se utiliza la función multiply del objeto functions para multiplicar y luego se imprime el resultado en la consola.
+console.log(functions.divide(2,0.9));//se utiliza la función divide del objeto functions para dividir y luego se imprime el resultado en la consola.
